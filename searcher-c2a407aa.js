bfa74cc44a8597436cc365bf752abeb801f38f41
septumfunk <jfleischman6@gmail.com>
2026-01-23 17:30:27 -0800

    Syntax

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4171e4d4.js',
+                path_to_root + 'searchindex-14f52038.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
